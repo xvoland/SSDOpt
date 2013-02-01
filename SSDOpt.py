febe4216fb94f09ce503a65f78c2faef4a01b9b4
Vitalii Tereshchuk <xvoland@dotoca.net>
2013-02-01 04:25:10 +0200

    init script

---
+++ SSDOpt.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+"""
+Setting OS X to store temporary files and files that are not important
+for the system in RAM rather than on disk.
+
+Based on the idea by:
+    - Ricardo Gameiro <http://blogs.nullvision.com/?p=357>
+    - Daniel Jenkins <http://blogs.nullvision.com/?p=357#comment-1140>
+
+*** ATTENTION !!! ***
+I DON'T TAKE ANY RESPONSIBILITY FOR THE CONSEQUENCES OF SCRIPT.
+HOPE YOU ENJOY USING THIS SCRIPT
+
+Good luck,
+    Vitalii Tereshchuk / [xVoLAnD]
+"""
