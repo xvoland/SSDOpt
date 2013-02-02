3a160f79cf26b79a1b100eb8cdbaae9dc7da2eb5
Vitalii Tereshchuk <xvoland@dotoca.net>
2013-02-02 05:19:03 +0200

    added function yes_no

--- SSDOpt.py
+++ SSDOpt.py
@@ -15,3 +15,58 @@ HOPE YOU ENJOY USING THIS SCRIPT
 Good luck,
     Vitalii Tereshchuk / [xVoLAnD]
 """
+
+import os
+import sys
+import platform
+import commands
+
+os.system('clear')
+
+print '********************************************************************'
+print '* SSD OPTIMIZATION v0.1 Installer for Mac OS X                     *'
+print '* I\'d be glad to see most of you at our page http://www.dotoca.net *'
+print '********************************************************************'
+print '\n\n'
+
+# check if running on Mac
+mac = (platform.system() == 'Darwin')
+
+# check if root, getuid doesn't return 0 if sudoing
+currentUser = commands.getoutput('whoami')
+if currentUser != 'root':
+    print 'Please re-run this installer with root privileges, i.e. "sudo python ./install.py"\n'
+    sys.exit()
+
+
+def yes_no(question, default='yes'):
+    """
+    Question to the user: (yes/no) or (y/n) not case sensitive
+
+    return: yes|no
+    """
+    valid = {'yes': 'yes',
+               'y': 'yes',
+              'no': 'no',
+               'n': 'no'
+    }
+
+    if default == None:
+        prompt = ' [y/n] '
+    elif default == 'yes':
+        prompt = ' [Y/n] '
+    elif default == 'no':
+        prompt = ' [y/N] '
+    else:
+        raise ValueError('Invalid default answer: "%s"' % default)
+
+    while 1:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return default
+        elif choice in valid.keys():
+            return valid[choice]
+        else:
+            sys.stdout.write('Please respond: "yes/no" or y/n.\n')
+
